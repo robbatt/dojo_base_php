3450aabab1fbbcea5527b2d6c1c8ede569de5fb3
Robert Brendler <robert.brendler@twofour.de>
2017-01-30 09:26:36 +0100

    unskip mock test (seems only broken in windows build of phpunit)

--- src/dojo/extended/ExtendedMockTest.php
+++ src/dojo/extended/ExtendedMockTest.php
@@ -14,7 +14,7 @@ class ExtendedMockTest extends TestCase
     /** @test */
     public function shouldMockIt()
     {
-        $this->markTestSkipped("phpunit mocking module seems broken, skip this in order to keep composer task 'test-repeat' running");
+        //$this->markTestSkipped("phpunit mocking module seems broken for windows, skip this in order to keep composer task 'test-loop' clean");
 
         /** @var \PHPUnit_Framework_MockObject_MockObject $mock */
         $mock = $this->createMock(FooModule::class);
